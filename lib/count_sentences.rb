6a7645d8f675b26db7c4bf91b1b8c1d4c1716835
cassymarie <cassy2205@gmail.com>
2020-11-02 15:24:27 -0700

    commit tree cc61e082386b004f677a6ccd8a1c8dc572f4418f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.strip.end_with?(".")
   end
 
   def question?
-
+    self.strip.end_with?("?")
   end
 
   def exclamation?
-
+    self.strip.end_with?("!")
   end
 
   def count_sentences
-
+    self.scan(/\w+\W?\s?\W?[.?!]/).size
   end
 end
